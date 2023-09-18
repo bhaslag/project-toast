597633eee206e6fcf7288673377ef3339729abb8
pc <pc@The-Mouse-House.local>
2023-09-18 12:12:46 +0200

    Finished exercise 6, cleaned the code up

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -2,25 +2,18 @@ import React from 'react';
 
 import Toast from '../Toast';
 import { ToastContext } from '../ToastProvider';
+import useEscapeKey from '../../hooks/use-escape-key.js';
 
 import styles from './ToastShelf.module.css';
 
 function ToastShelf({toasts}) {
   const { dismissAllToasts } = React.useContext(ToastContext);
 
-  React.useEffect(() => {
-    function handleKeyDown(event) {
-      if (event.code === 'Escape') {
-        dismissAllToasts();
-      }
-    }
+  const handleEscape = React.useCallback(() => {
+    dismissAllToasts();
+  }, []);
 
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    }
-  }, [dismissAllToasts]);
+  useEscapeKey(handleEscape);
 
   return (
     <ol 
